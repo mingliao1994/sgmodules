14fe0718db0d7758d31fb120ca870737d17c11d4
yangzichun <yangzichun@ykl-med.com>
2024-02-28 21:05:58 +0800

    add js

--- IPv6.js
+++
@@ -1,8 +0,0 @@
-var dnsResult = $request.dnsResult;
-var v6 = true;
-if(dnsResult.v6Addresses.length === 0) {
-    v6 = false;
-}
-var hostname = $request.hostname;
-var domainSuffixContains = hostname.indexOf('3lysia.com') >= 0 || hostname.indexOf('miss-pink-elf-elysia.com') >= 0;
-$done({matched: v6 && domainSuffixContains});
