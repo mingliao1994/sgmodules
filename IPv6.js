80ee7ac9b6ea364be1046c1fdcd46b9cfacd89b8
yangzichun <yangzichun@ykl-med.com>
2024-02-28 20:59:01 +0800

    add js

--- IPv6.js
+++ IPv6.js
@@ -1,9 +1,8 @@
-console.log('Starting to solve ip');
-
-var v6 = $request.sourceIP;
-console.log(v6);
-
 var dnsResult = $request.dnsResult;
-console.log(dnsResult);
-
-$done({matched: true});
+var v6 = true;
+if(dnsResult.v6Addresses.length === 0) {
+    v6 = false;
+}
+var hostname = $request.hostname;
+var domainSuffixContains = hostname.indexOf('3lysia.com') >= 0 || hostname.indexOf('miss-pink-elf-elysia.com') >= 0;
+$done({matched: v6 && domainSuffixContains});
