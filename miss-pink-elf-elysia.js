b45402aa91328091b490545183b7314f90de28e0
yangzichun <yangzichun@ykl-med.com>
2024-02-29 09:22:27 +0800

    add js

--- miss-pink-elf-elysia.js
+++ miss-pink-elf-elysia.js
@@ -4,4 +4,4 @@ const hostname = $request.hostname;
 const isV6Available = dnsResult.v6Addresses.length > 0;
 const isMatchingDomain = hostname.includes('3lysia.com') || hostname.includes('miss-pink-elf-elysia.com');
 
-$done({ matched: isV6Available && isMatchingDomain });
+$done({ matched: !isV6Available && isMatchingDomain });
